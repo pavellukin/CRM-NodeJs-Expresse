cf7f6de31f356fb7e38492f1498efb106b5ef4f6
Pavel Lukin <paveldon@web.de>
2019-12-11 17:09:10 +0100

    Das Verzeichnis Config wurde angelegt.

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+
 const analyticsRouter = require('./routes/analytics');
 const authRouter = require('./routes/auth');
 const categoryRouter = require('./routes/category');
 const orderRouter = require('./routes/order');
 const positionRouter = require('./routes/position');
-
+const keys = require('./config/keys');
 const app = express();
+
+mongoose.connect(keys.MONGO_URL)
+    .then(() => console.info('MongoDB connected.'))
+    .catch(err => console.error(err));
 
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URL: 'mongodb://pavel:abc123@ds353748.mlab.com:53748/fullstack' 
+};
