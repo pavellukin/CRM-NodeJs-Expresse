9a7fa9d97afa8446a743657810f34d73df9746d8
Pavel Lukin <paveldon@web.de>
2019-12-11 16:36:04 +0100

    Die Model-kassen wurden angelegt.

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const analyticsRouter = require('./routes/analytics');
 const authRouter = require('./routes/auth');
 const categoryRouter = require('./routes/category');
 const orderRouter = require('./routes/order');
 const positionRouter = require('./routes/position');
+
 const app = express();
 
+app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(require('cors')());
 
 app.use('/api/analytics', analyticsRouter);
 app.use('/api/auth', authRouter);
