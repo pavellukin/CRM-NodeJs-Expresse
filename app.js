2c66c5597c9787eaa5b3167863ef16d9c4d535e3
Pavel Lukin <paveldon@web.de>
2019-12-11 13:34:23 +0100

    Controllers und Routes wurden angelegt; Body-Parser wurde installiert.

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const analyticsRouter = require('./routes/analytics');
+const authRouter = require('./routes/auth');
+const categoryRouter = require('./routes/category');
+const orderRouter = require('./routes/order');
+const positionRouter = require('./routes/position');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/api/analytics', analyticsRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/category', categoryRouter);
+app.use('/api/order', orderRouter);
+app.use('/api/position', positionRouter);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-const express = require('express');
-const app = express();
+const app = require('./app');
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'working'
-    });
-
-});
-
-app.listen(5000, () => {
-    console.log('Server has been started');
-});
+app.listen(port, () => console.log(`Server has been started on ${port}`));
